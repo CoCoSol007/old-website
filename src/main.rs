490f742208f45725ae0dff8adc7d6459d66a6b38
CoCoSol007 <solois.corentin@gmail.com>
2023-12-06 16:01:13 +0100

    WIP: add article system

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 //! The main program of my website.
 
 use rocket::{
-    get, http::ContentType, launch, routes,Config,
-    Route,
+    get, http::ContentType, launch, routes,Config,post,
+    Route, FromForm, fs::TempFile, form::Form,
 };
+use uuid::Uuid;
+use std::env;
+
+
 
 /// a macro to define a route.
 macro_rules! raw_files {
@@ -28,6 +32,7 @@ raw_files! {
     "/about.html" => aboute_me_page(HTML, "../webpages/about.html"),
     "/articles.html" => articles_page(HTML, "../webpages/articles.html"),
     "/projects.html" => projects_page(HTML, "../webpages/projects.html"),
+    "/add_article.html" => add_article_page(HTML, "../webpages/add_article.html"),
     "/style.css" => style(CSS, "../webpages/style.css"),
     "/github-mark.png" => github_mark(SVG, "../webpages/github-mark.svg"),
     "/contact_me.png" => contact_me_mark(SVG, "../webpages/contact_me.svg"),
@@ -39,6 +44,40 @@ const fn favicon() -> (ContentType, &'static [u8]) {
     let content_type = ContentType::Icon;
 
     (content_type, ico_data)
+}
+
+// Article System API
+
+struct Articles {
+    articles: Vec<Article>,
+}
+
+/// An article.
+struct Article {
+    title: String,
+    intro: String,
+    id: String,
+}
+
+
+#[derive(FromForm)]
+struct Upload<'f> {
+    file: TempFile<'f>,
+    title: String, // the title of the article
+    intro: String, // a short description of the article
+}
+
+#[post("/upload", format = "multipart/form-data", data = "<form>")]
+async fn upload_file(mut form: Form<Upload<'_>>) -> std::io::Result<()> {
+
+
+    let file_id: String = Uuid::new_v4().hyphenated().encode_lower(&mut Uuid::encode_buffer()).to_owned();
+    let file_name =  env::current_dir().expect("Impossible de récupérer le répertoire actuel").display().to_string()+&String::from("\\data\\") + &file_id + ".md";
+
+    form.file.persist_to(file_name).await?;
+
+    Ok(())
+
 }
 
 
@@ -54,4 +93,5 @@ async fn rocket() -> _ {
         })
         .mount("/", raw_routes())
         .mount("/", routes![favicon])
+        .mount("/", routes![upload_file])
 }
