83f45f4a63307b2ee66731152e5af7acebb60529
CoCoSol007 <solois.corentin@gmail.com>
2023-12-10 16:41:48 +0100

    remove all

--- src/main.rs
+++ src/main.rs
@@ -26,25 +26,7 @@ macro_rules! raw_files {
 }
 
 raw_files! {
-    "/" => main_page(HTML, "../webpages/home.html"),
-    "/home.html" => wave(HTML, "../webpages/home.html"),
-    "/about.html" => aboute_me_page(HTML, "../webpages/about.html"),
-    "/articles.html" => articles_page(HTML, "../webpages/articles.html"),
-    "/projects.html" => projects_page(HTML, "../webpages/projects.html"),
-    "/add_article.html" => add_article_page(HTML, "../webpages/add_article.html"),
-    "/style.css" => style(CSS, "../webpages/style.css"),
-    "/github-mark.png" => github_mark(SVG, "../webpages/github-mark.svg"),
-    "/contact_me.png" => contact_me_mark(SVG, "../webpages/contact_me.svg"),
-    "/article/<_>" => article_page(HTML, "../webpages/article.html"),
-}
-
-/// a fonction to give the icon of the webpage.
-#[get("/favicon.ico")]
-const fn favicon() -> (ContentType, &'static [u8]) {
-    let ico_data: &'static [u8] = include_bytes!("../webpages/favicon.ico");
-    let content_type = ContentType::Icon;
-
-    (content_type, ico_data)
+    "/" => main_page(HTML, "../webpages/main.html"),
 }
 
 // The main function of the website.
@@ -57,10 +39,8 @@ async fn rocket() -> _ {
             // pas de output
             log_level: rocket::config::LogLevel::Critical,
             port: 80,
-            address: "192.168.1.86".parse().unwrap(),
             ..Default::default()
         })
         .mount("/", raw_routes())
-        .mount("/", routes![favicon])
         .mount("/api", routes![new_article, get_article_list, get_article, get_minia_article, get_random_article])
 }
